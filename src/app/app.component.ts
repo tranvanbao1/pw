f306fa6db7b19918eff4680cfd63c14cd0322b26
Brady Tran <baotrank22tpm4@gmail.com>
2020-10-20 15:03:35 +0700

    pw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+	password = '';
+	includeUseLetters = false;
+	includeUseNumbers = false;
+	includeUseSymbols = false;
+  
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+	onChangeUseLetters() {
+		this.includeUseLetters = !this.includeUseLetters;
+	}
+	onChangeUseNumbers() {
+		this.includeUseNumbers = !this.includeUseNumbers;
+	}
+	onChangeUseSymbols() {
+		this.includeUseSymbols = !this.includeUseSymbols;
+	}
+
+	onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if(this.includeUseLetters) {
+      validChars += letters;
+    }
+    if(this.includeUseNumbers) {
+      validChars += numbers;
+    }
+    if(this.includeUseSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i=0; i< this.length; i++) {
+      const index = Math.floor(Math.random() *validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+	}
 }
